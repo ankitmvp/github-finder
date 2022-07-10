66a6eb8d0bdc1425bfb8e3bc68f1f4bfd95598e1
Ankit Bansal <ankitbansalniit@outlook.com>
2022-07-10 20:16:18 +0530

    Initial commit.

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,8 @@
+import UserResults from "../users/UserResults";
+
+function Home(){
+    return <div>
+        <UserResults />
+    </div>
+}
+export default Home
---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { ClipLoader, HashLoader } from "react-spinners";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    setUsers(data);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <HashLoader
+        color="#808080"
+        loading={loading}
+        size={100}
+        className="mx-auto"
+      />
+    );
+  }
+}
+
+export default UserResults;
