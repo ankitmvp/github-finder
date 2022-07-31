9ecc892a3929e0da5e89de961c9e270363c45734
Ankit Bansal <ankitbansalniit@outlook.com>
2022-07-31 16:28:32 +0530

    Updated Action url 2

--- src/context/github/GithubAction.js
+++ src/context/github/GithubAction.js
@@ -13,8 +13,13 @@ export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
-  const response = await githubApi.get(`${GITHUB_URL}/search/users?${params}`);
-  return response.data.items;
+  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+  const { items } = await response.json();
+  return items;
 };
 
 export const getUser = async (login) => {
